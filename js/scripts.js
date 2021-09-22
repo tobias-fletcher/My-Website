4e1ff5bbfa03f68221a84190157e1e83178200b7
tobias-fletcher <tobias_fletcher@icloud.com>
2021-09-22 12:06:44 -0400

    adding modal

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,13 @@
         let container = input.parentElement;
 
         let error = container.querySelector('.error-message');
-        if(error) {
+        if (error) {
             container.removeChild(error);
         }
 
         if (message) {
             let error = document.createElement('div');
-            error.classList.add ('error-message');
+            error.classList.add('error-message');
             error.innerText = message;
             container.appendChild(error);
         }
@@ -21,12 +21,12 @@
     function validateEmail() {
         let value = emailInput.value;
 
-        if(!value) {
+        if (!value) {
             showErrorMessage(emailInput, 'Email is a required field.');
             return false;
         }
 
-        if(value.indexOf('@') === -1){
+        if (value.indexOf('@') === -1) {
             showErrorMessage(emailInput, 'You must enter a valid e-mail adddress.');
             return false;
         }
@@ -45,3 +45,28 @@
 
     emailInput.addEventListener('input', validateEmail);
 })();
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
