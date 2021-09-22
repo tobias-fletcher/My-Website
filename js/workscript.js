f2e1a224f7744d5f882a1de1decf5e6d40f51528
tobias-fletcher <tobias_fletcher@icloud.com>
2021-09-22 17:42:09 -0400

    fixed modal issue

--- js/workscript.js
+++ js/workscript.js
@@ -62,7 +62,7 @@ modalBtns.forEach(function (btn) {
 
 var closeBtns = document.querySelectorAll("modal-close");
 
-closeBtns.forEach(function (btn) {
+closeBtns.forEach(function () {
     window.onclick = function (event) {
         if (event.target === modal) {
             modal.style.display = "none";
