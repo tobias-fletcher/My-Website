d7095c8c699902c4f619cfffbc7f0f47a62b9e5e
tobias-fletcher <tobias_fletcher@icloud.com>
2021-09-22 14:09:20 -0400

    fixed modal issue

--- js/workscript.js
+++ js/workscript.js
@@ -25,7 +25,7 @@ window.onclick = function (event) {
 }
 
 // Get the modal
-var modal2 = document.getElementById("myModal2");
+var modal = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
@@ -35,17 +35,17 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function () {
-    modal2.style.display = "block";
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal2.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
-        modal2.style.display = "none";
+        modal.style.display = "none";
     }
 }
