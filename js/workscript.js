66c27a7f9874184d69fdd007d91d42ccd41e7a0e
tobias-fletcher <tobias_fletcher@icloud.com>
2021-09-22 15:16:47 -0400

    update script
    - update script for modals

--- js/workscript.js
+++ js/workscript.js
@@ -1,8 +1,8 @@
 // Get the modal
-var modal = document.querySelectorAll(".model");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.querySelectorAll("button.myBtn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -23,4 +23,3 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
-
