16fc149163609863fc3d8b5bcf221e7c02e3c82d
tobias-fletcher <tobias_fletcher@icloud.com>
2021-09-22 15:09:00 -0400

    fixed modal issue

--- js/workscript.js
+++ js/workscript.js
@@ -1,8 +1,8 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.querySelectorAll("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.querySelectorAll("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
