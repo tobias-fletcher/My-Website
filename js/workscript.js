22632c734ca49126c4bca632bb959454b2daef1d
tobias-fletcher <tobias_fletcher@icloud.com>
2021-09-23 22:16:01 -0400

    fixed modal issue

--- js/workscript.js
+++ js/workscript.js
@@ -62,6 +62,7 @@ modalBtns.forEach(function (btn) {
 });
 
 var span = document.getElementsByClassName("close");
+var modal;
 
 for (var i = 0; i < span.length; i++) {
     span[i].onclick = function () {
